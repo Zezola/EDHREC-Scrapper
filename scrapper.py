cfbfc2ce47eda8ab57e71063dd6a375e5eb0b272
Jose Ulisses <joseulisses03@gmail.com>
2025-07-16 19:50:53 -0300

    Recebendo URL via linha de comando pra fazer o scrapping

--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import argparse
 
-
+'''
+Escreve no arquivo csv chamado cards.csv a partir de uma lista de nomes
+'''
 def write_to_csv(card_names):
     with open('cards.csv', 'w', newline='') as csvfile:
         fieldnames = ['Nome']
@@ -10,9 +13,14 @@ def write_to_csv(card_names):
         writer.writeheader()
         writer.writerows(card_names)
 
-def scrape_hdrec_commander_page():
+'''
+Faz uma requisição GET no EDHREC e faz um scrapping buscando as cartas por nome. Como é commander por enquanto não precisamos nos preocupar
+nos nomes repetirem pq cada carta só pode ter uma no deck.
+'''
+def scrape_hdrec_commander_page(url):
     card_names = []
-    base_url = "https://edhrec.com/commanders/teysa-orzhov-scion"
+    #TODO: Talvez deixar uma base url fixa como commanders/
+    base_url = url
     # Fazer uma get pra pagina 
     response = requests.get(base_url)
     # O bs4 vai dar um parse no HTML
@@ -22,14 +30,24 @@ def scrape_hdrec_commander_page():
     for s in cards_span_elements: # Adicionar o text dos spans na lista de card_names
         card_name = s.text
         card_dict = {'Nome': card_name}
-        print(f"card_dict = {card_dict}\n")
+        #print(f"card_dict = {card_dict}\n")
         card_names.append(card_dict)
     return card_names
 
 
 
 def main():
+    # Initialize argpaser
+    argparser = argparse.ArgumentParser(description='Um script que faz um scrapping em uma pagina do EDHREC')
+    # Adicionando um arg posicional obrigatorio
+    argparser.add_argument('url', help='A url do deck que queremos ver.')
+    args = argparser.parse_args()
+    print(args.url)
+    """
     cards = scrape_hdrec_commander_page()
     write_to_csv(cards)
-
+    """
+    #TODO Antes de passar a URL, fazer um tratamento para verificar se essa URL existe e é do EDHREC (tambem verificar formato)
+    cards = scrape_hdrec_commander_page(args.url)
+    write_to_csv(cards)
 main()
