3bf1e70cc9699bb5acb5f8fb3c7bf83359360c56
Jose Ulisses <joseulisses03@gmail.com>
2025-07-16 20:56:10 -0300

    Versao inicial de pagina principal do projeto. Adicionando pasta pros decks. Extraindo nome do comandante da URL

--- scrapper.py
+++ scrapper.py
@@ -4,10 +4,23 @@ import csv
 import argparse
 
 '''
+Receber uma URL do tipo https://edhrec.com/commanders/belakor-the-dark-master
+E extrair só o nome do comandante
+Por enquanto vamos assumir que SEMPRE estamos recebendo um link de uma página de comandante. Adicionei no backlog uma tarefa de verificar se estamos
+na página de um comandante. 
+'''
+def extract_commander_name_from_url(url):
+    if len(url) == 0:
+        return None
+    commander_name = url.split("/")[-1]
+    print(commander_name)
+    return commander_name
+
+'''
 Escreve no arquivo csv chamado cards.csv a partir de uma lista de nomes
 '''
-def write_to_csv(card_names):
-    with open('cards.csv', 'w', newline='') as csvfile:
+def write_to_csv(card_names, commander_name):
+    with open(f"decks_by_commander/{commander_name}.csv", 'w', newline='') as csvfile:
         fieldnames = ['Nome']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
@@ -43,11 +56,13 @@ def main():
     argparser.add_argument('url', help='A url do deck que queremos ver.')
     args = argparser.parse_args()
     print(args.url)
+    commander_name = extract_commander_name_from_url(args.url)
+    print(commander_name)
     """
     cards = scrape_hdrec_commander_page()
     write_to_csv(cards)
     """
     #TODO Antes de passar a URL, fazer um tratamento para verificar se essa URL existe e é do EDHREC (tambem verificar formato)
     cards = scrape_hdrec_commander_page(args.url)
-    write_to_csv(cards)
+    write_to_csv(cards, commander_name)
 main()
